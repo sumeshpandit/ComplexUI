6cfd56fc0747b57b2f73a2d8181386912b0bcbb3
sumeshpandit <sumeshkpandit@gmail.com>
2021-05-21 00:18:52 +0530

    added more items

--- app/src/main/java/com/sumeshpandit/complexui/RecyclerAdapter.kt
+++ app/src/main/java/com/sumeshpandit/complexui/RecyclerAdapter.kt
@@ -25,6 +25,33 @@ class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>()
         data.add(GroceryItem(R.mipmap.item5, "super sale", 0,"EMAMI","Apple","1kg",201,4,87))
         data.add(GroceryItem(R.mipmap.item6, "", 17,"Rajdhani","Milk","1.5kg",534,5,0))
         data.add(GroceryItem(R.mipmap.item7, "super sale", 0,"Fresho","Grapes","5kg",199,2,32))
+
+        data.add(GroceryItem(R.mipmap.item8, "super sale", 13,"BB Royale","Almonds","5kg",1939,2,322))
+        data.add(GroceryItem(R.mipmap.item9, "", 34,"Amazon","Keyboard","1kg",1399,5,3212))
+
+        data.add(GroceryItem(R.mipmap.item10, "super sale", 43,"Flipkart","Mouse","1pc",1499,2,432))
+
+        data.add(GroceryItem(R.mipmap.item11, "super sale", 23,"Google","Laptop","2pc",5199,3,362))
+
+        data.add(GroceryItem(R.mipmap.item12, "super sale", 23,"Microsoft","Monitor","8pc",5199,5,392))
+
+        data.add(GroceryItem(R.mipmap.item13, "super sale", 67,"Apple","Tablet","12pc",1949,4,362))
+
+        data.add(GroceryItem(R.mipmap.item14, "", 45,"Ola","Phone","12oz",19239,5,362))
+
+        data.add(GroceryItem(R.mipmap.item15, "super sale", 67,"Uber","Scooter","2pc",19239,4,329))
+
+        data.add(GroceryItem(R.mipmap.item16, "super sale", 76,"Snapchat","Ice-cream","9kg",1199,3,832))
+
+        data.add(GroceryItem(R.mipmap.item17, "", 12,"Facebook","Water","200gm",1959,1,362))
+
+        data.add(GroceryItem(R.mipmap.item18, "", 76,"Bloomberg","Cold Drink","270gm",1969,4,322))
+
+        data.add(GroceryItem(R.mipmap.item19, "super sale", 45,"Adobe","Sprite","200gm",3199,6,352))
+
+        data.add(GroceryItem(R.mipmap.item20, "super sale", 12,"Cisco","AC","50kg",1979,4,332))
+
+
     }
 
     inner class ViewHolder(binding: ItemBinding):RecyclerView.ViewHolder(binding.root) {
@@ -68,9 +95,13 @@ class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>()
         holder.itemImage.setImageResource(data[position].itemImageView)
 
         val offer=data[position].offer
-        if(offer!="")
-            holder.offer.visibility=View.VISIBLE
-        holder.offer.text=offer
+        if(offer!="") {
+            holder.offer.text = offer
+            holder.offer.visibility = View.VISIBLE
+        }
+        else
+            holder.offer.visibility=View.GONE
+
 
         val off=data[position].off
         val offText="${off}% off"
