e3b112f1ac29f9a204b79c093f353d8b7bdd78ec
sumeshpandit <sumeshkpandit@gmail.com>
2021-05-20 23:50:31 +0530

    added gradients and corners

--- app/src/main/java/com/sumeshpandit/complexui/RecyclerAdapter.kt
+++ app/src/main/java/com/sumeshpandit/complexui/RecyclerAdapter.kt
@@ -2,7 +2,6 @@ package com.sumeshpandit.complexui
 
 import android.graphics.Color
 import android.graphics.Paint
-import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
